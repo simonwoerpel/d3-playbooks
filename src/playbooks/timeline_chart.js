b96b1331161bc2ce29d27f68fcecd54cff2a8bf3
Simon Wörpel <simon.woerpel@medienrevolte.de>
2016-11-17 03:05:42 +0100

    Apply tooltipEvents for `TimeLineChart`

--- src/playbooks/timeline_chart.js
+++ src/playbooks/timeline_chart.js
@@ -1,5 +1,6 @@
 import baseChart from './base_chart.js'
 import getScale from '../utils/d3/get_scale.js'
+import addTooltipEventsLine from '../utils/d3/add_tooltip_events_for_line.js'
 import drawLine from '../utils/d3/draw/line.js'
 
 export default class extends baseChart {
@@ -7,5 +8,6 @@ export default class extends baseChart {
     super()
     this.getXScale = getScale.bind({kind: 'scaleTime'})
     this.drawData = drawLine
+    this.addEvents = addTooltipEventsLine
   }
 }
