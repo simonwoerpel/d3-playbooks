480984a7f1c9e65bad18844ffa400fbf833f31f2
Simon Wörpel <simon.woerpel@medienrevolte.de>
2016-12-24 13:31:27 +0100

    refactor: Provider setter for charttype-wide opts

--- src/export.js
+++ src/export.js
@@ -5,27 +5,28 @@ import chart from './chart.js'
 // init
 d3.playbooks = {}
 
+const defaults = {global: {}}
+
 // set app-wide defaults
-let defaults = {}
 d3.playbooks.defaults = opts => {
-  defaults = opts
+  defaults['global'] = opts
 }
 
 // chart types as functions
 Object.keys(charts).map(c => {
+  defaults[c] = {}
 
   d3.playbooks[c] = opts => {
     const playbook = charts[c]
 
     // update opts, first global, then from chart type
-    opts = _(d3.playbooks[c]._defaults).merge(_(defaults)).merge(opts)
+    opts = _(defaults['global']).merge(_(defaults[c])).merge(_(opts))
     return chart({opts, playbook})
   }
 
   // add setter method for providing defaults
   // for given chart type
-  d3.playbooks[c]._defaults = {}
   d3.playbooks[c].defaults = opts => {
-    d3.playbooks[c]._defaults = opts
+    defaults[c] = opts
   }
 })
