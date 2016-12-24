0f9e77364d66bd8936bb7bd50a0985d2d9f3502b
Simon Wörpel <simon.woerpel@medienrevolte.de>
2016-12-24 13:53:47 +0100

    fix indent for examples

--- src/examples.js
+++ src/examples.js
@@ -1,33 +1,33 @@
 export default () => {
   d3.playbooks.barChart({
-      dataUrl: 'data/bar.csv'
+    dataUrl: 'data/bar.csv'
   }).build()
 
   d3.playbooks.horizontalBarChart({
-      dataUrl: 'data/bar.csv',
-      xCol: 'y',
-      yCol: 'x'
+    dataUrl: 'data/bar.csv',
+    xCol: 'y',
+    yCol: 'x'
   }).build()
 
   d3.playbooks.lineChart({
-      dataUrl: 'data/line.csv'
+    dataUrl: 'data/line.csv'
   }).build()
 
   d3.playbooks.scatterChart({
-      dataUrl: 'data/scatter.csv'
+    dataUrl: 'data/scatter.csv'
   }).build()
 
   d3.playbooks.timeLineChart({
-      dataUrl: 'data/timeline.csv'
+    dataUrl: 'data/timeline.csv'
   }).build()
 
   d3.playbooks.multiLineChart({
-      dataUrl: 'data/multiline.csv',
-      yCols: ['y1', 'y2', 'y3']
+    dataUrl: 'data/multiline.csv',
+    yCols: ['y1', 'y2', 'y3']
   }).build()
 
   d3.playbooks.multiTimeLineChart({
-      dataUrl: 'data/multitimeline.csv',
-      yCols: ['count_clinton', 'count_trump']
+    dataUrl: 'data/multitimeline.csv',
+    yCols: ['count_clinton', 'count_trump']
   }).build()
 }
