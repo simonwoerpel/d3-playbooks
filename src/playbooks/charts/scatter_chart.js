2ba5066b2e384f8767ead2afeecced18c685d682
Simon Wörpel <simon.woerpel@medienrevolte.de>
2016-12-30 23:15:33 +0100

    ScatterChart: start axes at zero by default

--- src/playbooks/charts/scatter_chart.js
+++ src/playbooks/charts/scatter_chart.js
@@ -1,8 +1,11 @@
+import getMaxDomain from '../../utils/domains/get_max_domain.js'
 import drawCircles from '../../utils/draw/circles.js'
 
 export default {
   plays: {
-    drawData: drawCircles
+    drawData: drawCircles,
+    getXDomain: getMaxDomain.bind({col: 'x', min: 0}),
+    getYDomain: getMaxDomain.bind({col: 'y', min: 0}),
   },
   defaults: {
     sizeRange: [3, 8],
