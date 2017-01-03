728f6d7b55206353a8a886163ecf5cfc12138860
Simon Wörpel <simon.woerpel@medienrevolte.de>
2017-01-03 10:40:53 +0100

    Add examples how to extend this library

--- src/examples.js
+++ src/examples.js
@@ -1,3 +1,21 @@
+// example how to add new functionality to this library
+
+// 1. create a function
+d3.playbooks.CHARTS.baseChart.plays.logWidth = C => console.log('width:', C.width)
+
+// 2. add new function block to playbook template
+// `d3.playbooks.TEMPLATE` is an `Immutable`: https://facebook.github.io/immutable-js/docs/#/OrderedMap
+const updatedTemplate = d3.playbooks.TEMPLATE
+  .mergeDeep({logger: {_width: 'logWidth'}})
+
+// 3. add this function block to the `resize` handler
+  .updateIn(['resize'], val => val.concat(['logger']))
+
+// 4. set new template as default `TEMPLATE`
+d3.playbooks.TEMPLATE = updatedTemplate
+
+
+// render some base examples
 export default () => {
   d3.playbooks.barChart({
     dataUrl: 'data/bar.csv'
