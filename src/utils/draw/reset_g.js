d456e2986d4b99f09fa291c990dff7c8d6a0d9d9
Simon Wörpel <simon.woerpel@medienrevolte.de>
2017-01-18 15:38:02 +0100

    Add flag to remove stuff outside of `g` element on resize & update.

--- src/utils/draw/reset_g.js
+++ src/utils/draw/reset_g.js
@@ -1,9 +1,12 @@
 // remove all children of the main `g` element
+// and all children of the base `element` that are flagged with a css class named `--delete-on-update`
 // prepare for re-drawing (e.g. during resize) with margins
 export default ({
   g,
+  element,
   margin
 }) => {
+  element.selectAll('.--delete-on-update').remove()
   g.selectAll('*').remove()
   g.attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
 }
